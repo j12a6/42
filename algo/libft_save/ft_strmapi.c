25909bfb1a4b18fa6ff2f315ff8ff727ee794904
Jean-Francois Aubert <jaubert@student.42.fr>
2014-01-22 16:29:13 +0100

    add libft_save

---
+++ algo/libft_save/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaubert <jaubert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/22 13:28:20 by jaubert           #+#    #+#             */
+/*   Updated: 2014/01/10 15:04:07 by jaubert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		i;
+
+	if (str == NULL || f == NULL)
+		return (NULL);
+	if (!(str = (char *)malloc(sizeof(*str) * (ft_strlen(s) + 1))))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ algo/libft_save/ft_strsplit.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaubert <jaubert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/01/10 15:07:41 by jaubert           #+#    #+#             */
+/*   Updated: 2014/01/10 15:26:33 by jaubert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+
+static int		ft_size_word(char const *s, char c, int i)
+{
+	int		m;
+
+	m = 0;
+	while (*(s + i++) != c && *(s + i++))
+		m++;
+	return (m);
+}
+
+static char		**ft_strsplit_next(char **s3, char const *s, char c)
+{
+	int		i;
+	int		j;
+	char	*s2;
+	int		k;
+
+	i = 0;
+	k = 0;
+	while (*(s + i))
+	{
+		while (*(s + i) == c && *(s + i))
+			i++;
+		if (*(s + i))
+		{
+			j = 0;
+			s2 = (char *)malloc(sizeof(*s2) * (ft_size_word(s, c, i) + 1));
+			while (*(s + i) != c && *(s + i))
+				*(s2 + j++) = *(s + i++);
+			*(s2 + j) = '\0';
+			*(s3 + k++) = s2;
+		}
+	}
+	*(s3 + k) = '\0';
+	return (s3);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	char	**s3;
+
+	if (s == NULL)
+	{
+		s3 = (char **)malloc(sizeof(*s3) * (1 + 1));
+		*s3 = NULL;
+		return (s3);
+	}
+	s3 = (char **)malloc(sizeof(*s3) * (ft_count_wd((char *)s, c) + 1));
+	return (ft_strsplit_next(s3, s, c));
+}
